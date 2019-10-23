a26eadc7adc9f53bacc052762ff64c4a532a9ab3
ist182069 <jose.bras@tecnico.ulisboa.pt>
2019-10-23 16:41:34 +0100

    Command Pattern

---
+++ Client/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDAD
+{
+    namespace Client
+    {
+        namespace Commands {
+            abstract class Command
+            {
+                public abstract void Execute(ClientCommunication clientCommunication, int port_int);
+            }
+        }        
+    }    
+}
