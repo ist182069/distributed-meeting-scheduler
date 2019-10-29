057298b09c7ecf54a698f1c78b432831fd460c78
Duarte Miguel <duarte.miguel@tecnico.ulisboa.pt>
2019-10-29 08:48:25 +0000

    ups, forgot these classes in previous commit

---
+++ Server/Location.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDAD
+{
+    public class Location
+    {
+        private String name;
+        private List<Room> rooms;
+
+        public Location(String name)
+        {
+            this.name = name;
+            this.rooms = new List<Room>();
+        }
+
+        public Location(String name, List<Room> rooms)
+        {
+            this.name = name;
+            this.rooms = rooms;
+        }
+
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public List<Room> getList()
+        {
+            return this.rooms;
+        }
+
+        public void AddRoom(Room room)
+        {
+            if (rooms.Contains(room))
+            {
+                return;
+            }
+            this.rooms.Add(room);
+        }
+    }
+}
---
+++ Server/Room.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDAD
+{
+    public class Room 
+    {
+        private String identifier;
+        private int capacity;
+        private bool reserved;
+
+        public Room(String identifier, int capacity)
+        {
+            this.identifier = identifier;
+            this.capacity = capacity;
+            this.reserved = false;
+        }
+
+        public String Identifier
+        {
+            get
+            {
+                return this.Identifier;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+        }
+
+        public bool Reserved
+        {
+            get
+            {
+                return this.reserved;
+            }
+        }
+
+    }
+}
