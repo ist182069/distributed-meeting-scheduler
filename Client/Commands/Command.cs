72d1f0f4874d13fe3d94182264521fbf8d65b9e2
Duarte Miguel <duarte.miguel@tecnico.ulisboa.pt>
2019-11-05 19:53:34 +0000

    uhh...

--- Client/Commands/Command.cs
+++ Client/Commands/Command.cs
@@ -6,36 +6,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MSDAD
+namespace MSDAD.Client.Commands
 {
-    namespace Client
-    {
-        namespace Commands {
             
-            abstract class Command
-            {
-                public int port;
-                public string ip, client_address;
-                public ClientLibrary clientLibrary;
-                public ServerInterface server;
+    abstract class Command
+    {
+        public int port;
+        public string ip, client_address;
+        public ClientLibrary clientLibrary;
+        public ServerInterface server;
 
-                public Command(ref ClientLibrary clientLibrary)
-                {
-                    this.clientLibrary = clientLibrary;
+        public Command(ref ClientLibrary clientLibrary)
+        {
+            this.clientLibrary = clientLibrary;
 
-                    Init();
-                }
+            Init();
+        }
 
-                void Init()
-                {                    
-                    this.port = this.clientLibrary.GetPort();
-                    this.ip = this.clientLibrary.GetIP();
-                    this.client_address = ClientUtils.AssembleClientAddress(ip, port);
-                    this.server = (ServerInterface)Activator.GetObject(typeof(ServerInterface), "tcp://localhost:11000/RemoteServer");
-                    this.server.Hello(this.ip, this.port);
-                }
-                public abstract object Execute();
-            }
-        }        
-    }    
+        void Init()
+        {                    
+            this.port = this.clientLibrary.GetPort();
+            this.ip = this.clientLibrary.GetIP();
+            this.client_address = ClientUtils.AssembleClientAddress(ip, port);
+            this.server = (ServerInterface)Activator.GetObject(typeof(ServerInterface), "tcp://localhost:11000/RemoteServer");
+            this.server.Hello(this.ip, this.port);
+        }
+        public abstract object Execute();
+    }
 }
