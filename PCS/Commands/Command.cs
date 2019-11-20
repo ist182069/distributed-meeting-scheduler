2b985dc3b8501de5d1386af553bc4e58debca7f8
ist182069 <jose.bras@tecnico.ulisboa.pt>
2019-11-20 18:05:55 +0000

    Comandos para manipular threads

--- PCS/Commands/Command.cs
+++ PCS/Commands/Command.cs
@@ -1,8 +1,33 @@
-﻿namespace MSDAD.PCS.Commands
-{
+﻿using System;
+using System.Runtime.InteropServices;
 
+namespace MSDAD.PCS.Commands
+{
     abstract class Command
     {
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        public static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr handle);
+
         public const string CLIENT = "Client";
         public const string CLIENT_EXE = "Client.exe";
 
--- PCS/Commands/Freeze.cs
+++ PCS/Commands/Freeze.cs
@@ -9,32 +9,7 @@ using System.Threading.Tasks;
 namespace MSDAD.PCS.Commands
 {
     class Freeze : Command
-    {
-        [Flags]
-        public enum ThreadAccess : int
-        {
-            TERMINATE = (0x0001),
-            SUSPEND_RESUME = (0x0002),
-            GET_CONTEXT = (0x0008),
-            SET_CONTEXT = (0x0010),
-            SET_INFORMATION = (0x0020),
-            QUERY_INFORMATION = (0x0040),
-            SET_THREAD_TOKEN = (0x0080),
-            IMPERSONATE = (0x0100),
-            DIRECT_IMPERSONATION = (0x0200)
-        }
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
-        [DllImport("kernel32.dll")]
-        static extern uint SuspendThread(IntPtr hThread);
-        [DllImport("kernel32.dll")]
-        static extern int ResumeThread(IntPtr hThread);
-        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
-        static extern bool CloseHandle(IntPtr handle);
-
-
-
+    {        
         string[] words;
         public Freeze(ref PCSLibrary pcsLibrary) : base(ref pcsLibrary)
         {
